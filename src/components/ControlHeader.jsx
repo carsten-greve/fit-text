3833b59962746c68604bb5a922536bcf53879166
carsten.greve@gmail.com <carsten.greve@gmail.com>
2026-01-22 19:33:00 +0100

    Move dropbox to separate component.

--- src/components/ControlHeader.jsx
+++ src/components/ControlHeader.jsx
@@ -1,51 +1,13 @@
-import { useState, useCallback, useEffect } from 'react';
-import { useApp } from '../AppProvider';
-import { Listbox, ListboxButton, ListboxOptions, Field, Label, Textarea } from '@headlessui/react';
-import { Image as ImageIcon, Type, Settings2, Download, Upload, FileText } from 'lucide-react';
-import { useDropzone } from 'react-dropzone';
+import { Listbox, ListboxButton, ListboxOptions, Textarea } from '@headlessui/react';
+import { Type } from 'lucide-react';
 import { SegmentControls } from './SegmentControls';
+import { Dropbox } from './Dropbox';
 
 const ControlHeader = () => {
-  const [file, setFile] = useState(null);
-  const { imageUrl, setImageUrl } = useApp();
-
-  const onDrop = useCallback((acceptedFiles) => {
-    if (acceptedFiles[0]) {
-      setFile(acceptedFiles[0]);
-      const url = URL.createObjectURL(acceptedFiles[0]);
-      console.log(`acceptedFiles[0]: ${acceptedFiles[0]}`);
-      console.log(`image url: ${url}`);
-      setImageUrl(url);
-    }
-  }, []);
-
-  useEffect(() => {
-    return () => {
-      if (imageUrl) URL.revokeObjectURL(imageUrl);
-    };
-  }, [imageUrl]);
-
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    onDrop,
-    accept: { 'image/png': ['.png'], 'image/jpeg': ['.jpeg, .jpg'] },
-    multiple: false
-  });
-
   return (
     <header className="flex h-30 items-center gap-4 border-b bg-white px-6 shadow-sm z-10">
-      
-      {/* Drop Box */}
-      <div 
-        {...getRootProps()} 
-        className={`flex-none border-2 border-dashed rounded-lg p-2 w-50 text-center transition-colors cursor-pointer flex items-center justify-center  ${isDragActive ? 'border-blue-500 bg-blue-50' : 'border-gray-300 bg-white hover:border-blue-400'}`}
-      >
-        <input {...getInputProps()} />
-        <Upload className="w-12 h-12 text-gray-400 mx-auto" />
-        <p className={`text-gray-600 ${file ? "truncate" : ""}`}>
-          {file ? `Selected: ${file.name}` : "Drag & drop optional background image here, or click to select"}
-        </p>
-      </div>
 
+      <Dropbox />
       <SegmentControls />
 
       {/* Text Input Area */}
---
+++ src/components/Dropbox.jsx
@@ -0,0 +1,44 @@
+import { useState, useCallback, useEffect } from 'react';
+import { useApp } from '../AppProvider';
+import { Upload } from 'lucide-react';
+import { useDropzone } from 'react-dropzone';
+
+export const Dropbox = () => {
+  const [file, setFile] = useState(null);
+  const { imageUrl, setImageUrl } = useApp();
+
+  const onDrop = useCallback((acceptedFiles) => {
+    if (acceptedFiles[0]) {
+      setFile(acceptedFiles[0]);
+      const url = URL.createObjectURL(acceptedFiles[0]);
+      console.log(`acceptedFiles[0]: ${acceptedFiles[0]}`);
+      console.log(`image url: ${url}`);
+      setImageUrl(url);
+    }
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      if (imageUrl) URL.revokeObjectURL(imageUrl);
+    };
+  }, [imageUrl]);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: { 'image/png': ['.png'], 'image/jpeg': ['.jpeg, .jpg'] },
+    multiple: false
+  });
+
+  return (
+    <div 
+      {...getRootProps()} 
+      className={`flex-none border-2 border-dashed rounded-lg p-2 w-50 text-center transition-colors cursor-pointer flex items-center justify-center  ${isDragActive ? 'border-blue-500 bg-blue-50' : 'border-gray-300 bg-white hover:border-blue-400'}`}
+    >
+      <input {...getInputProps()} />
+      <Upload className="w-12 h-12 text-gray-400 mx-auto" />
+      <p className={`text-gray-600 ${file ? "truncate" : ""}`}>
+        {file ? `Selected: ${file.name}` : "Drag & drop optional background image here, or click to select"}
+      </p>
+    </div>
+  );
+};
